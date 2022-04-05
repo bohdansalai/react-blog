5838114579e04cfdfe50e45eb4969b1eadc5d3ce
Bohdan <bohdansalai@gmail.com>
2022-04-05 18:57:09 +0200

    Made form controlable

--- src/components/BlogContent/components/AddPostForm.js
+++ src/components/BlogContent/components/AddPostForm.js
@@ -3,44 +3,63 @@ import "./AddPostForm.css";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const AddPostForm = ({ handleAddFormHide }) => {
-  return (
-    <>
-      <form className="addPostForm" action="">
-        <button className="hideBtn">
-          <FontAwesomeIcon
-            icon={faTimes}
-            onClick="handleAddFormHide"
-            size="xl"
-          />
-        </button>
-        <h2>Creating post</h2>
-        <div>
-          <input
-            className="addFormInput"
-            type="text"
-            name="postTitle"
-            placeholder="Title"
-          />
-        </div>
-        <div>
-          <textarea
-            className="addFormInput"
-            name="postDescription"
-            placeholder="Information "
-          />
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={handleAddFormHide}
-            className="blackBtn "
-          >
-            Add post
+export class AddPostForm extends React.Component {
+  state = {
+    postTitle: "",
+    postDesc: "",
+  };
+
+  handlePostTitleChange = (e) => {
+    this.setState({ postTitle: e.target.value });
+  };
+  handlePostDescChange = (e) => {
+    this.setState({ postDesc: e.target.value });
+  };
+
+  render() {
+    const handleAddFormHide = this.props.handleAddFormHide;
+    return (
+      <>
+        <form className="addPostForm" action="">
+          <button className="hideBtn">
+            <FontAwesomeIcon
+              icon={faTimes}
+              onClick="handleAddFormHide"
+              size="xl"
+            />
           </button>
-        </div>
-      </form>
-      <div className="overlay" onClick={handleAddFormHide}></div>
-    </>
-  );
-};
+          <h2>Creating post</h2>
+          <div>
+            <input
+              className="addFormInput"
+              type="text"
+              name="postTitle"
+              placeholder="Title"
+              value={this.state.postTitle}
+              onChange={this.handlePostTitleChange}
+            />
+          </div>
+          <div>
+            <textarea
+              className="addFormInput"
+              name="postDescription"
+              placeholder="Information "
+              value={this.state.postDescription}
+              onChange={this.handlePostDescChange}
+            />
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={handleAddFormHide}
+              className="blackBtn "
+            >
+              Add post
+            </button>
+          </div>
+        </form>
+        <div className="overlay" onClick={handleAddFormHide}></div>
+      </>
+    );
+  }
+}
