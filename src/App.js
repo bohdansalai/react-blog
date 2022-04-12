4d69ab61401cdfb079d9d4e85ad8f45ba935edfc
Bohdan <bohdansalai@gmail.com>
2022-04-12 14:53:49 +0200

    improved auth logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 import { Header } from "./components/Header/Header";
 import { BlogPage } from "./containers/BlogPage/BlogPage";
 import { Footer } from "./components/Footer/Footer";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { LoginPage } from "./containers/LoginPage/LoginPage";
 
 export function App() {
@@ -14,31 +19,50 @@ export function App() {
   const [userName, setUserName] = useState(localStorage.getItem("userName"));
 
   return (
-    <Router>
-      <div className="App">
-        <Header
-          userName={userName}
-          isLoggedIn={isLoggedIn}
-          setIsLoggedIn={setIsLoggedIn}
-        />
-        <main>
-          <Routes>
-            <Route exact path="/" element={<BlogPage />} />
-            <Route
-              path="/login"
-              element={
-                <LoginPage
-                  props
-                  setIsLoggedIn={setIsLoggedIn}
-                  setUserName={setUserName}
-                />
-              }
-            />
-          </Routes>
-        </main>
-        <Footer year={new Date().getFullYear()} />
-      </div>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <div className="App">
+          <Header
+            userName={userName}
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+          <main>
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  !isLoggedIn ? (
+                    <Navigate to="/login" />
+                  ) : (
+                    <Navigate to="/blog" />
+                  )
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  !isLoggedIn ? (
+                    <LoginPage
+                      setUserName={setUserName}
+                      setIsLoggedIn={setIsLoggedIn}
+                    />
+                  ) : (
+                    <Navigate to="/blog" />
+                  )
+                }
+              />
+              <Route
+                path="/blog"
+                element={!isLoggedIn ? <Navigate to="/login" /> : <BlogPage />}
+              />
+            </Routes>
+          </main>
+          <Footer year={new Date().getFullYear()} />
+        </div>
+      </Router>
+    </React.StrictMode>
   );
 }
 
