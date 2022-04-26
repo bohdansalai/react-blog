c639fa37a3b29396c617991956fb166531193259
Bohdan <bohdansalai@gmail.com>
2022-04-26 19:47:20 +0200

    Updates

---
+++ precache-manifest.fe025e785e57f2690d42ce9f343eace9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "541d0c6ec0c677cba371",
+    "url": "/react-blog/static/css/main.9409edd7.chunk.css"
+  },
+  {
+    "revision": "541d0c6ec0c677cba371",
+    "url": "/react-blog/static/js/main.541d0c6e.chunk.js"
+  },
+  {
+    "revision": "56dbc343fe2dac40479d",
+    "url": "/react-blog/static/js/1.56dbc343.chunk.js"
+  },
+  {
+    "revision": "31aedcd640fab93b3891",
+    "url": "/react-blog/static/js/2.31aedcd6.chunk.js"
+  },
+  {
+    "revision": "c09197eaaad2702c3d20",
+    "url": "/react-blog/static/js/runtime~main.c09197ea.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-blog/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5d23f5990db072aafbbc428030d1a06c",
+    "url": "/react-blog/index.html"
+  }
+];
