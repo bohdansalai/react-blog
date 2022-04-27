a275e7970dc6d1ba0c8d4aad47e01c6b7d36ce52
Bohdan <bohdansalai@gmail.com>
2022-04-27 20:33:45 +0200

    Updates

---
+++ precache-manifest.f26128571ad6e40e3cbbaf2cdc930032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f8edf46daa8458f1474",
+    "url": "/react-blog/static/css/main.9409edd7.chunk.css"
+  },
+  {
+    "revision": "5f8edf46daa8458f1474",
+    "url": "/react-blog/static/js/main.5f8edf46.chunk.js"
+  },
+  {
+    "revision": "56dbc343fe2dac40479d",
+    "url": "/react-blog/static/js/1.56dbc343.chunk.js"
+  },
+  {
+    "revision": "31aedcd640fab93b3891",
+    "url": "/react-blog/static/js/2.31aedcd6.chunk.js"
+  },
+  {
+    "revision": "c09197eaaad2702c3d20",
+    "url": "/react-blog/static/js/runtime~main.c09197ea.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-blog/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f0feb29a72775d50712c487eb90a840f",
+    "url": "/react-blog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/login/precache-manifest.2a0a21f5cbb8d986c5e0ffdafa89ff7d.js"
+  "/react-blog/precache-manifest.f26128571ad6e40e3cbbaf2cdc930032.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/login/index.html", {
+workbox.routing.registerNavigationRoute("/react-blog/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
